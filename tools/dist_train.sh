7d54212f777f431d5057f1abebd6ed9b3ee48197
Kiet Nguyen <kanguyen.vn@gmail.com>
2022-08-31 14:53:24 -0400

    Same

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -5,7 +5,7 @@ GPUS=1
 NODE_COUNT=8
 MASTER_ADDR=127.0.0.1
 MASTER_PORT=29500
-echo "rank: ${RANK}"
+# echo "rank: ${RANK}"
 echo "node count: ${NODE_COUNT}"
 echo "master addr: ${MASTER_ADDR}"
 echo "master port: ${MASTER_PORT}"
@@ -14,7 +14,6 @@ CONFIG=$1
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nnodes ${NODE_COUNT} \
-        # --node_rank ${RANK} \
         --master_addr ${MASTER_ADDR} \
         --master_port ${MASTER_PORT} \
         --nproc_per_node ${GPUS} \
