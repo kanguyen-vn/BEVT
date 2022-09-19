24ec154eb326da64fe463883e9be554d486cb750
Kiet Nguyen <kanguyen.vn@gmail.com>
2022-09-19 15:15:38 -0400

    Brought back srun

--- tools/slurm_train.sh
+++ tools/slurm_train.sh
@@ -13,11 +13,12 @@ SRUN_ARGS=${SRUN_ARGS:-""}
 PY_ARGS=${@:4}  # Any arguments from the forth one are captured by this
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-sbatch -p ${PARTITION} \
+srun -p ${PARTITION} \
     --job-name=${JOB_NAME} \
     --gres=gpu:${GPUS_PER_NODE} \
     --ntasks=${GPUS} \
     --ntasks-per-node=${GPUS_PER_NODE} \
     --cpus-per-task=${CPUS_PER_TASK} \
+    --kill-on-bad-exit=1 \
     ${SRUN_ARGS} \
     python -u tools/train.py ${CONFIG} --launcher="slurm" ${PY_ARGS}
