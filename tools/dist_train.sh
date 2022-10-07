d68ddb7492406f1b3557d8ab9bfe0045f8134d6a
Kiet Nguyen <kanguyen.vn@gmail.com>
2022-10-07 18:29:54 -0400

    Edited dist_train

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
-GPUS=1
+GPUS=`nvidia-smi -L | wc -l`
 
 RANK=0
-NODE_COUNT=8
-MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+NODE_COUNT=1
+MASTER_ADDR=127.0.0.1
 MASTER_PORT=29500
 echo "rank: ${RANK}"
 echo "node count: ${NODE_COUNT}"
 echo "master addr: ${MASTER_ADDR}"
 echo "master port: ${MASTER_PORT}"
-echo "node list: ${SLURM_NODELIST}"
-
-export MASTER_PORT=$MASTER_PORT
-export WORLD_SIZE=$NODE_COUNT
-export MASTER_ADDR=$MASTER_ADDR
 
 CONFIG=$1
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python -m torch.distributed.launch --nnodes ${NODE_COUNT} \
+        --node_rank ${RANK} \
         --master_addr ${MASTER_ADDR} \
         --master_port ${MASTER_PORT} \
         --nproc_per_node ${GPUS} \
